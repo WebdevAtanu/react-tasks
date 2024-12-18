31037e40afef0e850aef5f7d8b8c1dd5722fdaaa
Atanu <atanum991@gmail.com>
2024-12-18 10:48:17 +0530

    update

--- Contact Us-Landing/src/comps/input.jsx
+++
@@ -1,7 +0,0 @@
-export default function Input({type,id,place,...rest}){
-	return(
-		<>
-			<input {...rest} type={type} name={id} id={id} placeholder={place} className='border border-black p-2 w-full'/>
-		</>
-		)
-}
--- Contact Us-Landing/src/comps/textarea.jsx
+++
@@ -1,7 +0,0 @@
-export default function Area({...rest}){
-	return(
-		<>
-			<textarea {...rest} name="" id="" cols='40' rows='5' placeholder='Enter your query' className='border border-black resize-none p-2'></textarea>
-		</>
-		)
-}
